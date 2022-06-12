dd2e3ac5b967075228470dd2a9edb6ead304c28f
jglee96 <zakklee96@gmail.com>
2022-06-13 00:44:00 +0900

    [feat] WebSocket Server setup
    * http server expose 후 이를 WebSocket에서도 사용가능하도록 함

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,5 +9,10 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
