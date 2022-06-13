7a2e5c52571d318dc2ddc6822662fcf52d37c28f
jglee96 <zakklee96@gmail.com>
2022-06-14 07:49:45 +0900

    [feat] nickname 기능 추가
    * nickname event를 만들어서 server에서 socket에 nickname을 추가하도록
    구현
    
    TODO:
    * room 입장 전에 nickname을 설정하도록 해야함

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -17,7 +17,7 @@ function addMessage(message) {
 
 function handleMessageSubmit (event) {
     event.preventDefault();
-    const input = room.querySelector("input");
+    const input = room.querySelector("#msg input");
     const value = input.value;
     socket.emit("new_message", input.value, roomName, () => {
         addMessage(`You: ${value}`);
@@ -25,13 +25,21 @@ function handleMessageSubmit (event) {
     input.value = "";
 }
 
+function handleNicknameSubmit (event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
+
 function showRoom(msg) {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
-    const form = room.querySelector("form");
-    form.addEventListener("submit", handleMessageSubmit)
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit)
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -44,12 +52,12 @@ function handleRoomSubmit(event) {
 
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-    addMessage("someone joined");
+socket.on("welcome", (user) => {
+    addMessage(`${user} joined`);
 });
 
-socket.on("bye", () => {
-    addMessage("someone left");
+socket.on("bye", (user) => {
+    addMessage(`${user} left`);
 });
 
 socket.on("new_message", addMessage);
