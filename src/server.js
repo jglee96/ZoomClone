a0f0b79141b55e2d14330b9f48339f63baf410db
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-13 17:22:08 +0900

    [feat] 두 browser 간 chat 기능 구현
    * server에서 connect된 socket를 list로 가지고 있다가 messag가
      browser에서 입력되면 연결된 모든 browser에 해당 message send 하는 기능
    구현

--- src/server.js
+++ src/server.js
@@ -15,11 +15,14 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
     socket.on("message", message => {
-        console.log(message.toString('utf8'));
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
     });
     socket.send("hello!!");
 });
