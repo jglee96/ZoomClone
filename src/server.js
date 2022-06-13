9bb891d8e1ccae9638abb0d25dde9bf0dd0db095
jglee96 <zakklee96@gmail.com>
2022-06-14 07:34:20 +0900

    [feat] disconnecting, new_message event 구현
    * disconnecting: disconnect 이전 상태로 나가기 직전 이벤트 -> 다른
    사용자에게 나간다는 것을 알려줄 수 있음
    * new_message event를 구현하여 chat이 가능 하도록 구현
    * browser -> server, server -> browser 두 방향 모두 new_message event가
    있음

--- src/server.js
+++ src/server.js
@@ -22,6 +22,13 @@ wsServer.on("connection", socket => {
         done();
         socket.to(roomName).emit("welcome");
     });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach(room => socket.to(room).emit("bye"));
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+    });
 });
 
 // const wss = new WebSocket.Server({ server });
