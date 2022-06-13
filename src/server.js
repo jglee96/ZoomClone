e724a852332a5595a3f177d63330b84cd0f0b8f4
jonggeon_lee <jonggeon_lee@tmax.co.kr>
2022-06-13 17:52:53 +0900

    [feat] browser에서 nickname 표시하도록 구현
    * server에서 JSON을 parse하여 socket의 nickname을 저장함
    * socket에서 new_message가 들어오면 저장되어있는 nickname과 함께 다른
      browser에 보여줌

--- src/server.js
+++ src/server.js
@@ -19,12 +19,18 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", message => {
-        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    socket.on("message", msg => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
-    socket.send("hello!!");
 });
 
 server.listen(3000, handleListen);
