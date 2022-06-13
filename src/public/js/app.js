57fb565dfe7806ec55108338db9b3e07b13b0736
jglee96 <zakklee96@gmail.com>
2022-06-13 21:02:34 +0900

    [feat] socket join
    * browser에서 room name을 입력받아 server에서 room 생성
    * socket.join 으로 room에 join
    * callback 함수 실행하여 room 입장

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,13 +2,24 @@ const socket = io();
 
 const welcome = document.getElementById("welcome");
 const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function showRoom(msg) {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+}
 
 function handleRoomSubmit(event) {
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, () => {
-        console.log("server is done!");
-    });
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -14,11 +14,12 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", socket => {
-    socket.on("enter_room", (msg, done) => {
-        console.log(msg);
-        setTimeout(() => {
-            done();
-        }, 10000);
+    socket.onAny(event => {
+        console.log(`Socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
     });
 });
 
