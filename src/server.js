ab16fe63eef7fd787a951a45a4f358715dd43588
jglee96 <zakklee96@gmail.com>
2022-06-14 22:00:25 +0900

    [feat] public room 구별
    * 생성된 room 중 public room을 get하는 함수 구현`

--- src/server.js
+++ src/server.js
@@ -13,9 +13,21 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const { sockets: { adapter: { sids, rooms } } } = wsServer;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anon";
     socket.onAny(event => {
+        console.log(wsServer.sockets.adapter);
         console.log(`Socket Event: ${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
