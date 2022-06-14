a6b2fe44a9723eca2f9f34522d319aae99eed9cf
jglee96 <zakklee96@gmail.com>
2022-06-14 22:39:13 +0900

    [feat] socket.io의 admin panel 설정
    * socket.io의 admin panel 설정

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -11,7 +12,16 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+        origin: ["https://admin.socket.io"],
+        credentials: true,
+    }
+});
+
+instrument(wsServer, {
+    auth: false,
+});
 
 function publicRooms() {
     const { sockets: { adapter: { sids, rooms } } } = wsServer;
