73a883c21d8ef68fa952eca6bbbb0a339b9428fe
jglee96 <zakklee96@gmail.com>
2022-06-14 22:14:23 +0900

    [feat] 생성되어있는 room list 보여주는 기능 구현
    * room 변경시 room_change event를 server에서 보내면 browser에서 이를
    보여줌
    * 이때 socket.emit이 아니라 wsServer.sockets.emit을 하면 모든 사용자에게
    room change를 보여줌

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -61,3 +61,13 @@ socket.on("bye", (user) => {
 });
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", rooms => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    })
+});
